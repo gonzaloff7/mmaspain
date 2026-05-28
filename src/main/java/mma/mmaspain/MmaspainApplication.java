9ff61eb76bbd689e244c5149c7dd363ee61c8367
Gonzalo Fuentes <gonzalofuentesfigueroa7@gmail.com>
2026-05-28 17:08:11 +0200

    restore app

---
+++ src/main/java/mma/mmaspain/MmaspainApplication.java
@@ -0,0 +1,16 @@
+package mma.mmaspain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaspainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                MmaspainApplication.class,
+                args
+        );
+    }
+}
